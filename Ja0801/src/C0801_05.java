b3fa577f86e93fb3fd417835f346db324eb3ce61
minkim-korea <km24778@gmail.com>
2025-08-01 14:52:00 +0900

    0801

--- Ja0801/src/C0801_05.java
+++ Ja0801/src/C0801_05.java
@@ -4,28 +4,45 @@ public class C0801_05 {
 
 	public static void main(String[] args) {
 
-		// split 분리자를 통해 배열로 리턴
-		String str = "홍길동,유관순,이순신,강감찬,김구";
-		String[] name2 = str.split(",");
-		System.out.println(Arrays.toString(name2));
+		String[] strArr = { "a.txt", "b.hwp", "c.cls", "d.txt", "e.png", "f.jpg" };
+		// txt파일은jpg파일 몇개있는지 출력하시오
+		int count = 0;
+		for (int i = 0; i < strArr.length; i++) {
+			if (strArr[i].endsWith("txt") || strArr[i].endsWith("jpg")) {
+				System.out.println(strArr[i]);
+				count++;
+			}
+		}
+		System.out.println("txt파일 개수는" + count);
 
-		String str2 = "1,홍길동,100,100,100,300,100.0";
-		// split으로 분리한 다음 student 객체로 선언을 해보세요
-		// string -> int타입 ,Str타입 ,int타입 ....double타입으로 변경해서
-		// 객체선언해서 진행함 .
-		String[] title = str2.split(",");
-		int stuNo = Integer.parseInt(title[0]);
-		String name=title[1];
-		int kor = Integer.parseInt(title[2]);
-		int eng = Integer.parseInt(title[3]);
-		int math = Integer.parseInt(title[4]);
-		int total = Integer.parseInt(title[5]);
-		double avg= Double.parseDouble(title[6]);
-		//객체선언
-		Student stu = new Student(stuNo,name,kor,eng,math);
-		String[] title1 = { "번호", "이름", "국어", "영어", "수학", "합계", "평균" };
-		System.out.println(Arrays.toString(title1));
-		System.out.printf("%d\t%s\t %d\t %d\t %d\t %d\t %.2f \t",stu.getStuNo(),stu.getName(),stu.getKor(),stu.getEng(),stu.getMath(),stu.getTotal(),stu.getAvg());
+//		
+//        String str = "aaa.txt";
+//        // txt 로끝나냐 ?
+//        System.out.println(str.endsWith("txt")); 
+//		
+
+//		// split 분리자를 통해 배열로 리턴
+//		String str = "홍길동,유관순,이순신,강감찬,김구";
+//		String[] name2 = str.split(",");
+//		System.out.println(Arrays.toString(name2));
+//
+//		String str2 = "1,홍길동,100,100,100,300,100.0";
+//		// split으로 분리한 다음 student 객체로 선언을 해보세요
+//		// string -> int타입 ,Str타입 ,int타입 ....double타입으로 변경해서
+//		// 객체선언해서 진행함 .
+//		String[] title = str2.split(",");
+//		int stuNo = Integer.parseInt(title[0]);
+//		String name=title[1];
+//		int kor = Integer.parseInt(title[2]);
+//		int eng = Integer.parseInt(title[3]);
+//		int math = Integer.parseInt(title[4]);
+//		int total = Integer.parseInt(title[5]);
+//		double avg= Double.parseDouble(title[6]);
+//		//객체선언
+//		Student stu = new Student(stuNo,name,kor,eng,math);
+//		String[] title1 = { "번호", "이름", "국어", "영어", "수학", "합계", "평균" };
+//		System.out.println(Arrays.toString(title1));
+//		System.out.printf("%d\t%s\t %d\t %d\t %d\t %d\t %.2f \t",stu.getStuNo(),stu.getName(),stu.getKor(),stu.getEng(),stu.getMath(),stu.getTotal(),stu.getAvg());
 
 //	 //trim 빈공백제거 왼쪽오른쪽공백만(사이공백안됨)
 //		String str ="   abc    c";
