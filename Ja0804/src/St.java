29d4eba808f9f7d829d06163fcfe8f23c8f0a061
minkim-korea <km24778@gmail.com>
2025-08-04 18:28:56 +0900

    0804 컬렉션 Generics  파일입출력

---
+++ Ja0804/src/St.java
@@ -0,0 +1,14 @@
+
+
+public class St {
+
+	private int number;
+	private String name;
+	private String phone;
+	
+	St(int number,String name,String phone){
+		this.number = number;
+		this.name = name;
+		this.phone = phone;
+	}
+}
